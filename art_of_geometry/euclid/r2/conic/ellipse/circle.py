5d1b3eae34f255c3c8f306fdad56e8d3fa9df1fd
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-01-23 00:04:58 -0800

    add Euclid.R2.Conic.Ellipse.Circle

--- art_of_geometry/euclid/r2/conic/ellipse/circle.py
+++ art_of_geometry/euclid/r2/conic/ellipse/circle.py
@@ -1,5 +1,31 @@
+__all__ = \
+    'CircleInR2',
+
+
+from sympy.core.symbol import Symbol
 from sympy.geometry.ellipse import Circle as SymPyCircle
 
+from ..... import _GeometryEntityABC
+from ...coord import X, Y
+from ...point import PointInR2
 
-class CircleInR2:
-    pass
+
+class CircleInR2(SymPyCircle, _GeometryEntityABC):
+    def __new__(cls, center: PointInR2 = None, radius: Symbol = None, name: str = None):
+        return super().__new__(
+                cls,
+                center=center,
+                radius=radius)
+
+    def __init__(self, center: PointInR2 = None, radius: Symbol = None, name: str = None):
+        self.center = center
+
+        self.radius = radius
+
+        self._name = name
+
+    @property
+    def equation(self):
+        return (X - self.circle_in_r2.center.x) ** 2 \
+             + (Y - self.circle_in_r2.center.y) ** 2 \
+             - self.circle_in_r2.radius ** 2
