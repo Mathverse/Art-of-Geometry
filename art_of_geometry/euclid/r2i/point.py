a495c2b21b16b0e0b14c56bc6c30ca150b36f38c
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-01-22 23:18:14 -0800

    add Euclid.R2I.Point

---
+++ art_of_geometry/euclid/r2i/point.py
@@ -0,0 +1,16 @@
+from ..r2.point import PointInR2
+from . import _SurfaceInR2IABC
+from .coord import X, Y, W
+
+
+class PointInR2_DoubleConeInR2I(_SurfaceInR2IABC):
+    def __init__(self, point_in_r2: PointInR2):
+        assert isinstance(point_in_r2, PointInR2)
+
+        self.point_in_r2 = point_in_r2
+
+    @property
+    def equation(self):
+        return (X - self.point_in_r2.x) ** 2 \
+             + (Y - self.point_in_r2.y) ** 2 \
+             - W ** 2
