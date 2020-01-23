a1765830c288c9143e69a57b4d197924beada63a
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-01-22 23:24:46 -0800

    add Euclid.R2I.Point & .Line

---
+++ art_of_geometry/euclid/r2i/line.py
@@ -0,0 +1,18 @@
+from sympy.geometry.exceptions import GeometryError
+
+from ..r2.line import LineInR2
+from . import _SurfaceInR2IABC
+
+
+class LineInR2_VerticalPlaneInR2I(_SurfaceInR2IABC):
+    def __init__(self, line_in_r2: LineInR2):
+        assert isinstance(line_in_r2, LineInR2), \
+            GeometryError(
+            '*** {} NOT {} ***'
+                .format(line_in_r2, LineInR2.__name__))
+
+        self.line_in_r2 = line_in_r2
+
+    @property
+    def equation(self):
+        return self.line_in_r2.equation
--- art_of_geometry/euclid/r2i/point.py
+++ art_of_geometry/euclid/r2i/point.py
@@ -1,3 +1,9 @@
+__all__ = \
+    'PointInR2_DoubleConeInR2I',
+
+
+from sympy.geometry.exceptions import GeometryError
+
 from ..r2.point import PointInR2
 from . import _SurfaceInR2IABC
 from .coord import X, Y, W
@@ -5,7 +11,10 @@ from .coord import X, Y, W
 
 class PointInR2_DoubleConeInR2I(_SurfaceInR2IABC):
     def __init__(self, point_in_r2: PointInR2):
-        assert isinstance(point_in_r2, PointInR2)
+        assert isinstance(point_in_r2, PointInR2), \
+            GeometryError(
+                '*** {} NOT {} ***'
+                .format(point_in_r2, PointInR2.__name__))
 
         self.point_in_r2 = point_in_r2
 
