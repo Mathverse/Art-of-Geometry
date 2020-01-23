d44f992f9188c60231cf35aef0991c5669be1e17
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-01-23 09:54:03 -0800

    update Euclid.R2.Conic.Ellipse.Circle

--- art_of_geometry/euclid/r2/conic/ellipse/circle.py
+++ art_of_geometry/euclid/r2/conic/ellipse/circle.py
@@ -1,9 +1,10 @@
 __all__ = \
-    'CircleInR2',
+    'CircleInR2', 'CircleR2', 'Circle'
 
 
 from sympy.core.symbol import Symbol
 from sympy.geometry.ellipse import Circle as SymPyCircle
+from sympy.geometry.exceptions import GeometryError
 
 from ..... import _GeometryEntityABC
 from ...coord import X, Y
@@ -12,20 +13,40 @@ from ...point import PointInR2
 
 class CircleInR2(SymPyCircle, _GeometryEntityABC):
     def __new__(cls, center: PointInR2 = None, radius: Symbol = None, name: str = None):
+        assert isinstance(center, PointInR2), \
+            GeometryError(
+                '*** CENTER {} NOT {} ***'
+                .format(center, PointInR2.__name__))
+
         return super().__new__(
                 cls,
-                center=center,
-                radius=radius)
+                center,
+                radius)
 
     def __init__(self, center: PointInR2 = None, radius: Symbol = None, name: str = None):
-        self.center = center
+        # self.center = center
 
-        self.radius = radius
+        # self.radius = radius
 
         self._name = name
+
+    @property
+    def name(self):
+        return self._name \
+            if self._name \
+          else '{}({})'.format(
+                self.center.name,
+                self.radius)
+
+    def __repr__(self):
+        return 'Cir {}'.format(self.name)
 
     @property
     def equation(self):
         return (X - self.circle_in_r2.center.x) ** 2 \
              + (Y - self.circle_in_r2.center.y) ** 2 \
              - self.circle_in_r2.radius ** 2
+
+
+# aliases
+Circle = CircleR2 = CircleInR2
