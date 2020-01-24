3370d64b4b6d7f1cfa3baed75599e8756b3487aa
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-01-24 15:12:21 -0800

    refine type hints in _GeometryEntityABC

--- art_of_geometry/__init__.py
+++ art_of_geometry/__init__.py
@@ -4,7 +4,7 @@ __all__ = '_GeometryEntityABC',
 from abc import ABC, ABCMeta, abstractmethod, abstractproperty, abstractclassmethod, abstractstaticmethod
 from sympy.core.expr import Expr
 from sympy.geometry.entity import GeometryEntity
-from typing import List, Tuple
+from typing import Tuple
 
 
 class _GeometryEntityABC(GeometryEntity):
@@ -30,5 +30,5 @@ class _GeometryEntityABC(GeometryEntity):
     
     @property
     @abstractmethod
-    def parametric_equations(self) -> (List[Expr], Tuple[Expr, ...]):
+    def parametric_equations(self) -> Tuple[Expr, ...]:
         raise NotImplementedError
