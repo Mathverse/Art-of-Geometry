f955a01bce08d8bfdc6d3479cde68758f3e9d66e
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-01-23 23:56:21 -0800

    enhance Euclid.R2.Conic.ConicInR2

--- art_of_geometry/euclid/r2/conic/__init__.py
+++ art_of_geometry/euclid/r2/conic/__init__.py
@@ -1,12 +1,33 @@
 __all_ = 'ConicInR2', 'ConicR2', 'Conic'
 
 
+from sympy.core.symbol import Symbol
+
 from .. import _EuclidR2GeometryEntityABC
+from ..point import PointInR2
 
 
 class ConicInR2(_EuclidR2GeometryEntityABC):
-    def __init__(self):
-        pass
+    def __init__(self, focus: PointInR2, vertex: PointInR2, eccentricity: Symbol, name: str = None):
+        self.focus = focus
+
+        self.vertex = vertex
+
+        self.eccentricity = eccentricity
+
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name \
+            if self._name \
+          else '{}(vtx: {}, ecc: {})'.format(
+                self.focus.name,
+                self.vertex.name,
+                self.eccentricity)
+
+    def __repr__(self):
+        return 'Conic {}'.format(self.name)
 
 
 # aliases
