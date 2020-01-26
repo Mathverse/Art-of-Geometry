29afc485e8f8a0180d63d6f1fa12e8d657c5cf3f
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-01-25 17:02:37 -0800

    add more specific typing to Euclid.R2/R3._EuclidGeometryEntityABC.parametric_equations

--- art_of_geometry/euclid/r2/__init__.py
+++ art_of_geometry/euclid/r2/__init__.py
@@ -1,8 +1,16 @@
 __all__ = '_EuclidR2GeometryEntityABC',
 
 
+from abc import abstractmethod
+from functools import cached_property
+from sympy.core.expr import Expr
+from typing import Tuple
+
 from ... import _GeometryEntityABC
 
 
 class _EuclidR2GeometryEntityABC(_GeometryEntityABC):
-    pass
+    @cached_property
+    @abstractmethod
+    def parametric_equations(self) -> Tuple[Expr, Expr]:
+        raise NotImplementedError
--- art_of_geometry/euclid/r3/__init__.py
+++ art_of_geometry/euclid/r3/__init__.py
@@ -1,8 +1,16 @@
 __all__ = '_EuclidR3GeometryEntityABC',
 
 
+from abc import abstractmethod
+from functools import cached_property
+from sympy.core.expr import Expr
+from typing import Tuple
+
 from ... import _GeometryEntityABC
 
 
 class _EuclidR3GeometryEntityABC(_GeometryEntityABC):
-    pass
+    @cached_property
+    @abstractmethod
+    def parametric_equations(self) -> Tuple[Expr, Expr, Expr]:
+        raise NotImplementedError
