e5debbdc47a9f61b6624c66b97f7c0d4711c65b2
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-01-26 09:58:24 -0800

    minor fix to Point._PointABC

--- art_of_geometry/point.py
+++ art_of_geometry/point.py
@@ -3,12 +3,11 @@ __all__ = '_PointABC'
 
 from functools import cached_property
 from sympy.core.expr import Expr
-from sympy.geometry.point import Point
 
 from . import _GeometryEntityABC
 
 
-class _PointABC(_GeometryEntityABC, Point):
+class _PointABC(_GeometryEntityABC):
     @cached_property
     def distance_from_origin(self) -> Expr:
         raise NotImplementedError
