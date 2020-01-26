62385d32f4d385b365ac11a9f7237bb5be06822c
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-01-26 15:54:13 -0800

    update Euclid.R2I

--- art_of_geom/euclid/r2i/__init__.py
+++ art_of_geom/euclid/r2i/__init__.py
@@ -1,9 +1,21 @@
+__all__ = '_EuclidGeometryEntityInR2IABC', '_SurfaceInR2IABC'
+
+
+from abc import abstractmethod
+from functools import cached_property
+from sympy.core.expr import Expr
+from typing import Tuple
+
+
 from ... import _GeometryEntityABC
 
 
-class _EuclidR2IGeometryEntity(_GeometryEntityABC):
-    pass
+class _EuclidGeometryEntityInR2IABC(_GeometryEntityABC):
+    @cached_property
+    @abstractmethod
+    def parametric_equations(self) -> Tuple[Expr, Expr, Expr]:
+        raise NotImplementedError
 
 
-class _SurfaceInR2IABC(_GeometryEntityABC):
+class _SurfaceInR2IABC(_EuclidGeometryEntityInR2IABC):
     pass
--- art_of_geom/euclid/r2i/conic/ellipse/circle.py
+++ art_of_geom/euclid/r2i/conic/ellipse/circle.py
@@ -5,13 +5,13 @@ from sympy.geometry.exceptions import GeometryError
 
 from ....r2.conic.ellipse.circle import CircleInR2
 from ... import _SurfaceInR2IABC
-from ...coord import X, Y, W
+from ...coord import X, Y, Z
 
 
 class CircleInR2_HyperboloidInR2I(_SurfaceInR2IABC):
     def __init__(self, circle_in_r2: CircleInR2):
         assert isinstance(circle_in_r2, CircleInR2), \
-            GeometryError(
+            TypeError(
                 '*** {} NOT {} ***'
                 .format(circle_in_r2, CircleInR2.__name__))
 
@@ -21,5 +21,5 @@ class CircleInR2_HyperboloidInR2I(_SurfaceInR2IABC):
     def equation(self):
         return (X - self.circle_in_r2.center.x) ** 2 \
              + (Y - self.circle_in_r2.center.y) ** 2 \
-             - W ** 2 \
+             - Z ** 2 \
              - self.circle_in_r2.radius ** 2
