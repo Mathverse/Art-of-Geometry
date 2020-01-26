2500b10a04abb8508bd27c3c50173ab63d5754ff
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-01-25 16:39:38 -0800

    update Euclid.R2I coord

--- art_of_geometry/euclid/r2i/coord.py
+++ art_of_geometry/euclid/r2i/coord.py
@@ -1,3 +1,4 @@
+from sympy.core.numbers import I
 from sympy.core.symbol import Symbol
 
 from ..r2.coord import X, Y
@@ -5,3 +6,6 @@ from ..r2.coord import X, Y
 
 # coordinate of imaginary W axis
 W = Symbol(name='w', real=True)
+
+# corresponding complex coordinate
+_Z = Y + I * W
