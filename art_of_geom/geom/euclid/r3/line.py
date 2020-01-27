2da890d7c53d4e4eeb0a003b4432860335c83605
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-01-27 10:59:41 -0800

    refactor Geom.Euclid.R3.Line to use Py3.7

--- art_of_geom/geom/euclid/r3/line.py
+++ art_of_geom/geom/euclid/r3/line.py
@@ -38,7 +38,7 @@ class _LineInR3ABC(_LinearEntityInR3ABC, _EuclidLineABC):
 
 
 class LineInR3(_LineInR3ABC, _EuclidConcreteLineABC, Line3D):
-    def __new__(cls, point_0: PointInR3, point_1: _PointInR3ABC, /, *, name: str = None) -> Line3D:
+    def __new__(cls, point_0: PointInR3, point_1: _PointInR3ABC, *, name: str = None) -> Line3D:
         assert isinstance(point_0, PointInR3), \
             TypeError(
                 '*** POINT_0 {} NOT OF TYPE {} ***'
@@ -69,7 +69,7 @@ class LineInR3(_LineInR3ABC, _EuclidConcreteLineABC, Line3D):
                     '*** POINT_1 {} NEITHER {} NOR {} ***'
                     .format(point_1, PointInR3.__name__, PointAtInfinityInR3.__name__))
 
-    def __init__(self, point_0: PointInR3, point_1: _PointInR3ABC, /, *, name: str = None) -> None:
+    def __init__(self, point_0: PointInR3, point_1: _PointInR3ABC, *, name: str = None) -> None:
         self.point_0 = point_0
 
         self.point_1 = point_1
@@ -87,13 +87,13 @@ class LineInR3(_LineInR3ABC, _EuclidConcreteLineABC, Line3D):
                Y - self.point_0.y - self.direction.y * T, \
                Z - self.point_0.z - self.direction.z * T
 
-    def parallel_line(self, through_point: PointInR3, /, *, name=None):
+    def parallel_line(self, through_point: PointInR3, *, name=None):
         return LineInR3(
                 through_point,
                 PointAtInfinityInR3(self.direction),
                 name=name)
 
-    def perpendicular_line(self, through_point: PointInR3, /, *, name=None):
+    def perpendicular_line(self, through_point: PointInR3, *, name=None):
         # TODO: ASSUME through_point NOT ON THIS LINE
 
         return LineInR3(
@@ -116,7 +116,7 @@ LnAtInf = LineAtInf = LineAtInfinity = LineAtInfinityR3 = LineAtInfinityInR3
 
 
 class RayInR3(_LinearEntityInR3ABC, _EuclidRayABC, Ray3D):
-    def __new__(cls, point_0: PointInR3, point_1: _PointInR3ABC, /, *, name: str = None) -> Ray3D:
+    def __new__(cls, point_0: PointInR3, point_1: _PointInR3ABC, *, name: str = None) -> Ray3D:
         assert isinstance(point_0, PointInR3), \
             TypeError(
                 '*** POINT_0 {} NOT OF TYPE {} ***'
@@ -147,7 +147,7 @@ class RayInR3(_LinearEntityInR3ABC, _EuclidRayABC, Ray3D):
                     '*** POINT_1 {} NEITHER {} NOR {} ***'
                     .format(point_1, PointInR3.__name__, PointAtInfinityInR3.__name__))
 
-    def __init__(self, point_0: PointInR3, point_1: _PointInR3ABC, /, *, name: str = None) -> None:
+    def __init__(self, point_0: PointInR3, point_1: _PointInR3ABC, *, name: str = None) -> None:
         self.point_0 = point_0
 
         self.point_1 = point_1
@@ -165,7 +165,7 @@ Ray = RayR3 = RayInR3
 
 
 class SegmentInR3(_LinearEntityInR3ABC, _EuclidSegmentABC, Segment3D):
-    def __new__(cls, point_0: PointInR3, point_1: PointInR3, /, *, name: str = None) -> Segment3D:
+    def __new__(cls, point_0: PointInR3, point_1: PointInR3, *, name: str = None) -> Segment3D:
         assert isinstance(point_0, PointInR3), \
             TypeError(
                 '*** POINT_0 {} NOT OF TYPE {} ***'
@@ -181,7 +181,7 @@ class SegmentInR3(_LinearEntityInR3ABC, _EuclidSegmentABC, Segment3D):
                 p1=point_0,
                 p2=point_1)
 
-    def __init__(self, point_0: PointInR3, point_1: PointInR3, /, *, name: str = None) -> None:
+    def __init__(self, point_0: PointInR3, point_1: PointInR3, *, name: str = None) -> None:
         self.point_0 = point_0
 
         self.point_1 = point_1
