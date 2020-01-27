bce26176a01c84838a78a641260a55cc46c914af
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-01-26 17:11:20 -0800

    add place-holder Geom.NonEuclid.Mobius & .Torus

---
+++ art_of_geom/geom/non_euclid/mobius/surface.py
@@ -0,0 +1,8 @@
+__all__ = '_MobiusSurfaceABC',
+
+
+from ..surface import _NonEuclidSurfaceABC
+
+
+class _MobiusSurfaceABC(_NonEuclidSurfaceABC):
+    pass
---
+++ art_of_geom/geom/non_euclid/torus/surface.py
@@ -0,0 +1,8 @@
+__all__ = '_TorusSurfaceABC',
+
+
+from ..surface import _NonEuclidSurfaceABC
+
+
+class _TorusSurfaceABC(_NonEuclidSurfaceABC):
+    pass
