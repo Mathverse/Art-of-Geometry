cf2fb0c4a48b035b2da080a71855e342bfd1c447
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-01-26 17:07:39 -0800

    refactor Geom.NonEuclid.Surface

--- art_of_geom/geom/non_euclid/surface.py
+++ art_of_geom/geom/non_euclid/surface.py
@@ -4,5 +4,9 @@ __all__ = '_NonEuclidSurfaceABC',
 from ..abc import _GeometryEntityABC
 
 
-class _NonEuclidSurfaceABC(_GeometryEntityABC):
+class _NonEuclidSpaceABC:
+    pass
+
+
+class _NonEuclidSurfaceABC(_NonEuclidSpaceABC, _GeometryEntityABC):
     pass
