30a696665082aeb8e5b91d97430c4a7aafc45073
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-02-13 15:42:26 -0800

    print Py ver in doc conf

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3.8
+
+
 # Configuration file for the Sphinx documentation builder
 # This file only contains a selection of the most common options
 # For a full list see the documentation: https://www.sphinx-doc.org/en/master/usage/configuration.html
@@ -5,7 +8,11 @@
 
 from datetime import date
 import os
-import sys
+from sys import version_info
+
+
+# Python version
+print(f"\n*** BUILDING DOCUMENTATION USING PYTHON {version_info[:2]} ***\n")
 
 
 # -- Path setup --------------------------------------------------------------
