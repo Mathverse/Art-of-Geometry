1989a834564e57c948fc02c6fb4560f952e60593
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-02-17 11:40:15 -0800

    minor edits to _Util._Compat

--- art_of_geom/_util/_compat.py
+++ art_of_geom/_util/_compat.py
@@ -3,10 +3,8 @@ __all__ = 'cached_property',
 
 from sys import version_info
 
-from .. import _MIN_PY_VER
 
-
-if version_info >= _MIN_PY_VER:
+if version_info >= (3, 8):
     from functools import cached_property
 
 else:
