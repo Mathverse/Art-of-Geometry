bad8bcdeca2e8d4acb6881b3a58709ef4c8fa5cb
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-02-19 10:19:27 -0800

    add Geom.Euclid._R1.CoOrd

---
+++ art_of_geom/geom/euclid/_r1/coord.py
@@ -0,0 +1,8 @@
+__all__ = 'X',
+
+
+from sympy.core.symbol import Symbol
+
+
+# coordinates of real X axis
+X = Symbol(name='x', real=True)
--- art_of_geom/geom/euclid/r2/coord.py
+++ art_of_geom/geom/euclid/r2/coord.py
@@ -1,6 +1,9 @@
+__all__ = 'X', 'Y'
+
+
 from sympy.core.symbol import Symbol
+from .._r1.coord import X
 
 
-# coordinates of real X & Y axes
-X = Symbol(name='x', real=True)
+# coordinates of real Y axis
 Y = Symbol(name='y', real=True)
