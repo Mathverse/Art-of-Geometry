9cc6c5a2bb8910b7dd2c337e3854ba312be96f22
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-02-18 23:43:20 -0800

    add Geom.Euclid._R0._ABC._Point

--- art_of_geom/geom/euclid/_r0/_abc/_point.py
+++ art_of_geom/geom/euclid/_r0/_abc/_point.py
@@ -1,4 +1,13 @@
-__all__ = ()
+__all__ = 'PointInR0', 'PointR0', 'Point', 'Pt'
 
 
 from ..._abc._point import _EuclideanConcretePointABC
+
+
+@_EuclideanConcretePointABC.assign_name_and_dependencies
+class _PointInR0(_EuclideanConcretePointABC):
+    pass
+
+
+# singleton aliases
+Pt = Point = PointR0 = PointInR0 = _PointInR0(name='SINGULARITY')
