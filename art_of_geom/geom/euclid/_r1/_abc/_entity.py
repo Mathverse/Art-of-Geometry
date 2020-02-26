23eb190661080ff6965cb6c88d70eb93304311d8
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-02-25 19:06:40 -0800

    add Geom.Euclid._R1._ABC._Entity._EuclideanGeometryEntityInR1

---
+++ art_of_geom/geom/euclid/_r1/_abc/_entity.py
@@ -0,0 +1,8 @@
+__all__ = '_EuclideanGeometryEntityInR1ABC',
+
+
+from ..._abc._entity import _EuclideanGeometryEntityABC
+
+
+class _EuclideanGeometryEntityInR1ABC(_EuclideanGeometryEntityABC):
+    pass
---
+++ art_of_geom/geom/euclid/_r1/sphere.py
@@ -0,0 +1,31 @@
+__all__ = \
+    'SphereInR1', 'SphereR1', 'Sphere', \
+    'CircleInR1', 'CircleR1', 'Circle'
+
+
+from ...._util._compat import cached_property
+from ...var import Variable
+from ._abc._entity import _EuclideanGeometryEntityInR1ABC
+from .point import PointInR1
+
+
+class SphereInR1(_EuclideanGeometryEntityInR1ABC):
+    def __init__(self, center: PointInR1, radius: Variable) -> None:
+        self.center = center
+        self.radius = radius
+
+    @cached_property
+    def point_0(self) -> PointInR1:
+        return PointInR1(self.center - self.radius)
+
+    @cached_property
+    def point_1(self) -> PointInR1:
+        return PointInR1(self.center + self.radius)
+
+    @cached_property
+    def points(self):
+        return self.point_0, self.point_1
+
+
+# aliases
+Circle = CircleR1 = CircleInR1 = Sphere = SphereR1 = SphereInR1
