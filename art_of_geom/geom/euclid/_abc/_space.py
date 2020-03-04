da5ade7b21233e25fb4415b3ae8768dd5f791944
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-03-03 19:07:55 -0800

    add place-holder Geom.Euclid._ABC._Space

---
+++ art_of_geom/geom/euclid/_abc/_space.py
@@ -0,0 +1,12 @@
+__all__ = '_EuclideanSpaceABC', '_EuclideanHalfSpaceABC'
+
+
+from ..._abc._space import _SpaceABC, _HalfSpaceABC
+
+
+class _EuclideanSpaceABC(_SpaceABC):
+    pass
+
+
+class _EuclideanHalfSpaceABC(_HalfSpaceABC):
+    pass
