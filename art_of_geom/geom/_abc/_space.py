ce10c0dd8a1efb2eaef4069ef85dcc684d7065ce
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-03-03 18:37:51 -0800

    add Geom._ABC._Space._HalfSpaceABC

--- art_of_geom/geom/_abc/_space.py
+++ art_of_geom/geom/_abc/_space.py
@@ -2,7 +2,14 @@ __all__ = '_SpaceABC',
 
 
 from ._entity import _GeometryEntityABC
+from ._point import _ConcretePointABC
 
 
 class _SpaceABC(_GeometryEntityABC):
     pass
+
+
+class _HalfSpaceABC(_GeometryEntityABC):
+    def __init__(self, boundary: _SpaceABC, normal_vector: _ConcretePointABC) -> None:
+        self.boundary = boundary
+        self.normal_vector = normal_vector
