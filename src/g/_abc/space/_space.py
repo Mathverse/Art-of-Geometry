6113cd91a7fcd249ad0947c185439674ebe1d0d5
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-04-21 13:41:41 -0700

    refactor g._abc.space

--- src/g/_abc/space/_space.py
+++ src/g/_abc/space/_space.py
@@ -1,15 +1,23 @@
 __all__ = '_SpaceABC', '_HalfSpaceABC'
 
 
+from dataclasses import dataclass
+
 from ._entity import _GeometryEntityABC
 from ._point import _ConcretePointABC
 
 
 class _SpaceABC(_GeometryEntityABC):
-    pass
+    """Abstract Space."""
 
 
-class _HalfSpaceABC(_GeometryEntityABC):
-    def __init__(self, boundary: _SpaceABC, point: _ConcretePointABC) -> None:
-        self.boundary = boundary
-        self.point = point
+class _SubSpaceABC(_GeometryEntityABC):
+    """Abstract Sub-Space."""
+
+
+@dataclass
+class _HalfSpaceABC(_SubSpaceABC):
+    """Abstract Half Space with a specified Boundary and containing a Point."""
+
+    boundary: _SpaceABC
+    point: _ConcretePointABC
