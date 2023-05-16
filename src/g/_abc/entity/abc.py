6a54961d2a19eccc67b4090b7f2a6dad1c1785ae
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-05-16 16:56:42 -0700

    refactor g._abc.entity.abc._EntityABC

--- src/g/_abc/entity/abc.py
+++ src/g/_abc/entity/abc.py
@@ -68,7 +68,7 @@ class _EntityABC:
 
     @cache
     @classmethod
-    def __class_full_name__(cls: type, /) -> str:
+    def __class_full_name__(cls: type, /) -> LiteralString:
         return f'{cls.__module__}.{cls.__qualname__}'
 
     @property
