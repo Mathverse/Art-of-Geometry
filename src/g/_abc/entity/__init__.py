dc2b3c1b0dba1325cfbfb7ae5c704d36da52abd4
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-05-16 14:29:09 -0700

    refactor g._abc.entity submodule

---
+++ src/g/_abc/entity/__init__.py
@@ -0,0 +1,14 @@
+"""Entity."""
+
+
+from collections.abc import Sequence
+from typing import LiteralString
+
+from .abstract import _EntityABC, assign_entity_name_and_dependencies
+from .geometry import _GeometryEntityABC
+
+
+__all__: Sequence[LiteralString] = (
+    '_EntityABC', 'assign_entity_name_and_dependencies',
+    '_GeometryEntityABC',
+)
