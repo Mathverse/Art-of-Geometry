afcbae0a0e77873df053e9a77cd51a7d45d7f48a
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-05-16 15:48:40 -0700

    update g._art frontend submodule

--- src/g/_art/__init__.py
+++ src/g/_art/__init__.py
@@ -1,1 +1,11 @@
-"""Art/Graphics Rendering Engines."""
+"""Art Frontends."""
+
+
+from collections.abc import Sequence
+from typing import LiteralString
+
+from .abc import _ArtFrontendABC
+from .manim import MAnimFrontend
+
+
+__all__: Sequence[LiteralString] = '_ArtFrontendABC', 'MAnimFrontend'
--- src/g/_art/manim.py
+++ src/g/_art/manim.py
@@ -1,1 +1,14 @@
-"""MAnim Rendering Engine."""
+"""MAnim Art Frontend."""
+
+
+from collections.abc import Sequence
+from typing import LiteralString
+
+from .abc import _ArtFrontendABC
+
+
+__all__: Sequence[LiteralString] = ('MAnimFrontend',)
+
+
+class MAnimFrontend(_ArtFrontendABC):
+    """MAnim Art Frontend."""
