9e307ffa25daa7d1df1507e84351f6359842900a
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-05-16 19:52:27 -0700

    update g._abc.vector submodule

--- src/g/_abc/vector.py
+++ src/g/_abc/vector.py
@@ -1,1 +1,14 @@
 """Abstract Vector."""
+
+
+from collections.abc import Sequence
+from typing import LiteralString
+
+from .entity import _GeomEntityABC
+
+
+__all__: Sequence[LiteralString] = ('_VectorABC',)
+
+
+class _VectorABC(_GeomEntityABC):
+    """Abstract Vector."""
