c1d5302b7faf0f24ae6e936294040984f8151794
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-05-17 20:29:46 -0700

    update g._abc.vector._VectorABC

--- src/g/_abc/vector.py
+++ src/g/_abc/vector.py
@@ -2,13 +2,18 @@
 
 
 from collections.abc import Sequence
+from dataclasses import dataclass
 from typing import LiteralString
 
+from .._util.type import Num
 from .entity import _GeomEntityABC
 
 
 __all__: Sequence[LiteralString] = ('_VectorABC',)
 
 
+@dataclass
 class _VectorABC(_GeomEntityABC):
     """Abstract Vector."""
+
+    coordinates: tuple[Num]
