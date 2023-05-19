4c5ad644cb59549692a2b8d930c06f103755f69f
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-05-19 14:54:03 -0700

    add g._abc.coord_sys submodule

---
+++ src/g/_abc/coord_sys.py
@@ -0,0 +1,12 @@
+"""Abstract Coordinate System."""
+
+
+from collections.abc import Sequence
+from typing import LiteralString
+
+
+__all__: Sequence[LiteralString] = ('_CoordinateSystemABC',)
+
+
+class _CoordinateSystemABC:
+    """Abstract Coordinate System."""
