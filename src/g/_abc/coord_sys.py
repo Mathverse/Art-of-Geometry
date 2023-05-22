8de0bcea24ee38db338582d1ccdb8167053155b0
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-05-22 10:21:23 -0700

    update g._abc.coord_sys._CoordinateSystemABC

--- src/g/_abc/coord_sys.py
+++ src/g/_abc/coord_sys.py
@@ -1,8 +1,14 @@
 """Abstract Coordinate System."""
 
 
+from abc import abstractmethod
 from collections.abc import Sequence
 from typing import LiteralString
+
+from ..variable import RealVarOrNum
+
+from .point import _PointABC
+from .space.abc import _SpaceABC
 
 
 __all__: Sequence[LiteralString] = ('_CoordinateSystemABC',)
@@ -10,3 +16,8 @@ __all__: Sequence[LiteralString] = ('_CoordinateSystemABC',)
 
 class _CoordinateSystemABC:
     """Abstract Coordinate System."""
+
+    @abstractmethod
+    def __call__(self, point: _PointABC, space: _SpaceABC) -> tuple[RealVarOrNum]:  # noqa: E501
+        """Return coordinate of Point in Space."""
+        raise NotImplementedError
