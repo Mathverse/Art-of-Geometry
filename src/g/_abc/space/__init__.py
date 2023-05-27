8d90ec0d350ce5cca35f277ba84937f67a88e96c
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-05-27 10:21:37 -0700

    update g._abc.space module docstring

--- src/g/_abc/space/__init__.py
+++ src/g/_abc/space/__init__.py
@@ -1,1 +1,13 @@
-from .euclid import r2, r2i, r3
+"""Abstract Space base classes.
+
+In this Art of Geometry package, Spaces are set-like geometric entities
+containing Points and in/on which Points can be moved/transformed.
+
+The 3-dimensional Euclidean space, 2-dimensional Euclidean flat planes,
+2-dimensional spherical/elliptical/hyperbolical surfaces, 1-dimensional
+Euclidean straight lines, 1-dimensional circles and conic curves are all
+considered Spaces. A Space hence naturally contains lower-dimensional Spaces.
+
+Spaces do not have "hard" boundaries that "prevent" Points from moving across.
+For example, half-planes or hemispheres are not considered Spaces.
+"""
