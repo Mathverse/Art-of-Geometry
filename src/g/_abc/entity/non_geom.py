3c17c71448c77d305e118eb08282210e0367f551
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-05-27 10:03:20 -0700

    update g._abc.entity.non_geom module docstring

--- src/g/_abc/entity/non_geom.py
+++ src/g/_abc/entity/non_geom.py
@@ -1,8 +1,8 @@
 """Abstract Non-Geometric Entity.
 
-Examples of non-geometric entities include numerical Variables and Vectors,
-which represent directions and magnitudes/strengths but are not tied to any
-location in space.
+Examples of non-geometric entities include numerical Variables, which represent
+numbers, and Vectors, which represent directions and magnitudes/strengths but
+are not tied to any location in space.
 """
 
 
