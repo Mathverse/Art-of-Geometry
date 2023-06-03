ed5959b86d035f369c8badb7860a98ad1a125845
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-06-02 22:58:39 -0700

    update g._abc.space submodule docstring

--- src/g/_abc/space/__init__.py
+++ src/g/_abc/space/__init__.py
@@ -3,6 +3,10 @@
 In this Art of Geometry package, Spaces are set-like geometric entities
 containing Points and in/on which Points can be moved/transformed.
 
+This Art of Geometry package primarily deals with SMOOTHLY CONTINUOUS Spaces,
+in/on which Points' movements/transformations are characterized by smoothly/
+differentiably continuous functions and/or coordinate/parametric equations.
+
 The 3-dimensional Euclidean space, 2-dimensional Euclidean flat planes,
 2-dimensional spherical/elliptical/hyperbolical surfaces, 1-dimensional
 Euclidean straight lines, 1-dimensional circles and conic curves are all
