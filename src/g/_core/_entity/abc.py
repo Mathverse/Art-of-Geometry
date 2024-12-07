de44fa19117cd882f1f5255107c5100357116f08
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-12-06 23:04:13 -0800

    minor fix to imports in src.g._core.entity.abc sub-module

--- src/g/_core/_entity/abc.py
+++ src/g/_core/_entity/abc.py
@@ -3,10 +3,10 @@
 
 from __future__ import annotations
 
+from abc import abstractmethod
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from abc import abstractmethod
     from collections.abc import Sequence
     from typing import LiteralString, Self
 
