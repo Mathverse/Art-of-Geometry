c791bd13b8502026ae5eec3bad5957fe77a14517
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-12-07 19:16:47 -0800

    add src.g._core.coord_sys.type.Coords type

---
+++ src/g/_core/coord_sys/type.py
@@ -0,0 +1,18 @@
+"""Coordinates types."""
+
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+    from typing import LiteralString
+
+    from ..variable import NumOrVar
+
+
+__all__: Sequence[LiteralString] = ('Coords',)
+
+
+Coords: type = tuple[NumOrVar] | dict[str, NumOrVar]
