8ecb25a66260b88a167e5f0bb0a91a53abfad80d
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-12-10 14:12:33 -0800

    refactor class-method object inspection example

--- src/g/_core/_entity/decor/examples/class-method.py
+++ src/g/_core/_entity/decor/examples/class-method.py
@@ -1,4 +1,4 @@
-"""Inspect Class Method."""
+"""Class-Method Object Inspection."""
 
 
 from __future__ import annotations
@@ -9,14 +9,21 @@ from pprint import pprint
 from g._util.inspect import is_class_method, describe
 
 
-class C:
+class Cls:
     """A Class."""
 
     @classmethod
-    def m(cls) -> None:
+    def cls_method(cls) -> None:
         """A Class Method."""  # noqa: D401
 
 
-print(f'{C.m} Is Method? {ismethod(C.m)}')
-print(f'{C.m} Is Class Method? {is_class_method(C.m)}')
-pprint(describe(C.m))
+print(f'{Cls.cls_method} Is Method? {ismethod(Cls.cls_method)}')
+print(f'{Cls.cls_method} Is Class Method? {is_class_method(Cls.cls_method)}')
+pprint(describe(Cls.cls_method))
+
+
+inst: Cls = Cls()
+
+print(f'{inst.cls_method} Is Method? {ismethod(inst.cls_method)}')
+print(f'{inst.cls_method} Is Class Method? {is_class_method(inst.cls_method)}')
+pprint(describe(inst.cls_method))
