f6546164f82483f7ae99839f2d3f5188a62e1cfe
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-12-10 00:04:42 -0800

    add property-object inspection example

---
+++ src/g/_core/_entity/decor/examples/property.py
@@ -0,0 +1,24 @@
+"""Inspect Property."""
+
+
+from __future__ import annotations
+
+from pprint import pprint
+from typing import TYPE_CHECKING
+
+from g._util.inspect import is_property, describe
+
+if TYPE_CHECKING:
+    from typing import Self
+
+
+class C:
+    """A Class."""
+
+    @property
+    def p(self: Self) -> None:
+        """A Property"""  # noqa: D401
+
+
+print(f'{C.p} Is Property? {is_property(C.p)}')
+pprint(describe(C.p))
