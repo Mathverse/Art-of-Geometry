ebefb11ccdb7e34f7ceb660d014219002fcdcf5d
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-12-14 09:16:02 -0800

    refactor generator-object inspection example

--- src/g/_core/_entity/decor/examples/generator.py
+++ src/g/_core/_entity/decor/examples/generator.py
@@ -1,8 +1,13 @@
 """Generator Object Inspection."""
 
 
-from collections.abc import Generator
+from __future__ import annotations
+
 from inspect import isgenerator
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from collections.abc import Generator
 
 
 gen: Generator[int, None, None] = (i for i in range(0))  # noqa: UP043
