93ea1094831da467762b2149cbd55a53915db415
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-12-15 21:08:36 -0800

    refactor instance-method object inspection example

--- src/g/_util/inspect/examples/instance-method.py
+++ src/g/_util/inspect/examples/instance-method.py
@@ -3,7 +3,7 @@
 
 from __future__ import annotations
 
-from inspect import isfunction, ismethod
+from inspect import isfunction, ismethod, isroutine
 from pprint import pprint
 from typing import TYPE_CHECKING
 
@@ -20,16 +20,15 @@ class Cls:
         """An Instance Method."""  # noqa: D401
 
 
-print(f'{Cls.meth} Is Function? {isfunction(Cls.meth)}')
-print(f'{Cls.meth} Is Bound Instance Method? {ismethod(Cls.meth)}')
-print(f'{Cls.meth} Is Bound Instance Method? {is_instance_method(Cls.meth, bound=True)}')  # noqa: E501
 print(f'{Cls.meth} Is Unbound Instance Method? {is_instance_method(Cls.meth, bound=False)}')  # noqa: E501
+print(f'{Cls.meth} Is Function? {isfunction(Cls.meth)}')
+print(f'{Cls.meth} Is Routine? {isroutine(Cls.meth)}')
+pprint(describe(Cls.meth))
 
 
 inst: Cls = Cls()
 
-print(f'{inst.meth} Is Function? {isfunction(inst.meth)}')
-print(f'{inst.meth} Is Bound Instance Method? {ismethod(inst.meth)}')
 print(f'{inst.meth} Is Bound Instance Method? {is_instance_method(inst.meth, bound=True)}')  # noqa: E501
-print(f'{inst.meth} Is Unbound Instance Method? {is_instance_method(inst.meth, bound=False)}')  # noqa: E501
+print(f'{inst.meth} Is Bound Instance Method? {ismethod(inst.meth)}')
+print(f'{inst.meth} Is Routine? {isroutine(inst.meth)}')
 pprint(describe(inst.meth))
