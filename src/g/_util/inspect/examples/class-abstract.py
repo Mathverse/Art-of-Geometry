6e92765d8a30afc68abb5b1ed5092ac01da5a100
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-12-15 21:13:25 -0800

    refactor abstract-base-class inspection example

--- src/g/_util/inspect/examples/class-abstract.py
+++ src/g/_util/inspect/examples/class-abstract.py
@@ -6,15 +6,19 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from inspect import isabstract, isclass
 from pprint import pprint
+from typing import TYPE_CHECKING
 
 from g._util.inspect import describe
+
+if TYPE_CHECKING:
+    from typing import Self
 
 
 class ACls(ABC):
     """An Abstract Base Class."""
 
     @abstractmethod
-    def ab_meth(self) -> None:
+    def ab_meth(self: Self) -> None:
         """An Abstract Method."""  # noqa: D401
 
 
